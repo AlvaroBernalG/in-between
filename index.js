09a86ae9a999c20472d072c6727df1ee89fe0de3
Alvaro <alvaro.bernal.gimeno@gmail.com>
2017-06-25 10:28:58 +0100

    Refactoring

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
-const { or, every } = require('logical-operators')
+const { some } = require('logical-operators')
 
-const validAlphabetCharacters = '[a-z]|[A-Z]'
+const validAlphabetCharacters = '^[a-z]$|^[A-Z]$'
 
 const is = {
-  alphabetCharacter (target) {
-    return !!target.match(validAlphabetCharacters)
-  },
-  string (target) {
-    return target instanceof String || typeof target === 'string'
-  },
-  number (target) {
-    return typeof target === 'number'
-  },
-  date (target) {
-    return target instanceof Date
-  },
-  singleChar (target) {
-    return target.length === 1
-  }
+  alphabet: (target) => !!target.match(validAlphabetCharacters),
+
+  number: (target) => typeof target === 'number',
+
+  date: (target) => target instanceof Date
 }
 
 const inRange = (start, end, target) => target > start && target < end
 
 const between = (start, end) => (target) => {
-  const args = [start, end, target]
+  const valid = some(is.date, is.number, is.alphabet)
 
-  const string_alphabet_singleChar = every(is.string, is.alphabetCharacter, is.singleChar)
-
-  if (args.every(string_alphabet_singleChar)) {
-    return inRange(start, end, target)
-  }
-
-  const dateOrNumber = or(is.date, is.number)
-
-  if (args.every(dateOrNumber)) {
+  if ([start, end, target].every(valid)) {
     return inRange(start, end, target)
   }
 
